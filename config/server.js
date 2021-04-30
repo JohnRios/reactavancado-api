deafac2dabe90bccf4e12de6c48fa0737e153327
John Rios <joao.paulo.rios@hotmail.com>
2021-04-30 18:09:18 -0300

    feat: create strapi-app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '90a76adc3b664de987a36627ac42c879'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c4bf890e-62b3-44e0-8cf1-9a4eadced0c2'
+};
